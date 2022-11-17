503e2274f6292f01f6de9a32f735b172ba5525d8
NasrElsisi <84046654+NasrElsisi@users.noreply.github.com>
2022-11-17 18:41:33 +0200

    AuthorCreateView

--- books/urls.py
+++ books/urls.py
@@ -6,5 +6,6 @@ app_name = "books"
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
     path("authors/", views.AuthorListView.as_view(), name="list_authors"),
+    path("author/add", views.AuthorCreateView.as_view(), name="add_author"),
     path("authors/<int:pk>", views.AuthorDetailView.as_view(), name="author_detail"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
 from books.models import Author, Book
+from django.contrib import messages
+
 
 class HomeView(TemplateView):
     template_name = "books/home.html"
@@ -9,7 +11,23 @@ class HomeView(TemplateView):
 class AuthorListView(ListView):
     model = Author
     template_name = "books/author_list.html"
-    
+
+
 class AuthorDetailView(DetailView):
     model = Author
     template_name = "books/author_detail.html"
+
+
+class AuthorCreateView(CreateView):
+    model = Author
+    template_name = "books/author_create.html"
+    fields = ["name"]
+    success_url = ""
+
+    def form_valid(self, form):
+        messages.add_message(
+            self.request,
+            messages.SUCCESS,
+            "The author has been added",
+        )
+        return super().form_valid(form)
